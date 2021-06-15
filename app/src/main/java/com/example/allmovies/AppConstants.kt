ebbdd298116ffd8f43e22eac5a762efc84c3aa46
igor lacourt <igorlacourt@gmail.com>
2021-06-14 21:02:15 -0300

    remove api key

--- app/src/main/java/com/example/allmovies/AppConstants.kt
+++ app/src/main/java/com/example/allmovies/AppConstants.kt
@@ -1,6 +1,6 @@
 package com.example.allmovies
 
 object AppConstants {
-    const val TMDB_API_KEY = "fef98cf6bd829f53836bb7d92b02d6ef"
+    const val TMDB_API_KEY = ""
     const val BASE_URL = "https://api.themoviedb.org/3/"
 }
